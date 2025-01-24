620b788d864c215f92d142abca708753625746b6
Alex Colby <alexanderdcolby@gmail.com>
2025-01-24 22:39:13 +0000

    docs(alacritty): properly documents clear keybind

--- modules/terminal/alacritty.nix
+++ modules/terminal/alacritty.nix
@@ -9,6 +9,8 @@
     settings = {
       font.normal.family = "MesloLGL Nerd Font";
       keyboard.bindings = [
+        # Clear terminal via a keybind,
+	# works even halfway through a command
         { key = "Delete"; mods = "Control"; chars = "\\u000c"; }
       ];
     };
