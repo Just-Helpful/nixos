0a17053929ed70f55dfe461b481676f19a4b44ed
Alex Colby <alexanderdcolby@gmail.com>
2025-01-24 16:54:20 +0000

    feat(git): migrates existing git config

--- modules/base/git.nix
+++ modules/base/git.nix
@@ -9,7 +9,26 @@
     };
 
     extraConfig = {
+      # merge by default
       pull.rebase = false;
+
+      # start on "main" branch
+      init.defaultBranch = "main";
+
+      # automatically add branches when pushing
+      push.autoSetupRemote = true;
+      push.default = current;
+
+      # nice colouring and line numbers while diffing
+      diff.colorMoved = "default";
+      core.line-numbers = true;
+
+      # better automatic merging; honestly it's a bit hard to explain,
+      # see the [git docs](https://git-scm.com/book/en/v2/Git-Tools-Rerere)
+      rerere.enabled = true;
+
+      # have the most recent branches on top when changing branch
+      sort = "-committerdate";
     };
   };
 
