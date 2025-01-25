20e1562f66b6e9f56ca6820855e630d5a9b79741
Alex Colby <alexanderdcolby@gmail.com>
2025-01-25 13:53:17 +0000

    feat(keys): adds simple keybinds

---
+++ modules/desktop/keybinds.nix
@@ -0,0 +1,13 @@
+{ ... }: {
+  dconf.settings = {
+    "org/gnome/desktop/wm/keybindings" = {
+      # Close windows easier
+      close = ["<Super>q"];
+      # Disable "move-monitor" keybinds
+      move-to-monitor-up = [];
+      move-to-monitor-down = [];
+      move-to-monitor-left = [];
+      move-to-monitor-right = [];
+    };
+  };
+}
