c2e3839351492a55d8eee305137b5ea2e6b06c16
Alex Colby <alexanderdcolby@gmail.com>
2025-01-27 17:36:06 +0000

    docs: adds source for nvim config

--- modules/terminal/nvim/default.nix
+++ modules/terminal/nvim/default.nix
@@ -2,6 +2,8 @@
 {
   imports = [ inputs.nvf.homeManagerModules.default ];
 
+  # Neovim flake options
+  # mostly sourced from the default [config](https://github.com/NotAShelf/nvf/blob/main/configuration.nix)
   programs.nvf.enable = true;
   programs.nvf.settings.vim = {
     # Only allow `nvim` command
