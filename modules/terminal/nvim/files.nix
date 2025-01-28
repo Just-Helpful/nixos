edf9ac9cd25b287e21adf62b5ca0ad76b1aa7ac9
Alex Colby <alexanderdcolby@gmail.com>
2025-01-28 15:17:13 +0000

    feat: adds project management

--- modules/terminal/nvim/files.nix
+++ modules/terminal/nvim/files.nix
@@ -2,6 +2,7 @@
 _: {
   programs.nvf.enable = true;
   programs.nvf.settings.vim = {
+    projects.project-nvim.enable = true;
     filetree.neo-tree.enable = true;
     tabline.nvimBufferline.enable = true;
     visuals.nvim-web-devicons.enable = true;
