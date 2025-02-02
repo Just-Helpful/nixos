354858c6e79c8610d8509684521ae30002c1043f
Alex Colby <alexanderdcolby@gmail.com>
2025-02-02 18:34:40 +0000

    fix(alacritty): removes duplicate installation

--- hosts/default/users/home.nix
+++ hosts/default/users/home.nix
@@ -11,7 +11,6 @@
     # The home.packages option allows you to install Nix packages into your
     # environment.
     packages = with pkgs; [
-      alacritty
       treefmt2
       gnome-tweaks
     ];
