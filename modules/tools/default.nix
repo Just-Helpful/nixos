dd1c7817a4de07661a26bb356ececf35c71632d3
Alex Colby <alexanderdcolby@gmail.com>
2025-02-02 18:35:43 +0000

    tweak(tools): adds `yq`

--- modules/tools/default.nix
+++ modules/tools/default.nix
@@ -2,10 +2,15 @@
 # These tools provide the basis for command line manipulation.
 # I'll be expanding it when I encounter a use case either in the raw command line
 # or in the nvim editor, after I've tried them out via `nix-shell`
-_: {
+{ pkgs, ... }:
+{
   imports = [
     ./fzf.nix
     ./git.nix
     ./gpg.nix
+  ];
+
+  home.packages = with pkgs; [
+    yq
   ];
 }
