de5e8cff758007e5d329f539f1741c375da9f166
Alex Colby <alexanderdcolby@gmail.com>
2025-02-12 22:18:14 +0000

    feat(vscode): uses installed nerd font

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -1,5 +1,12 @@
 _: {
+  imports = [
+    ../themes/fonts.nix
+  ];
+
   programs.vscode = {
     enable = true;
+    userSettings = {
+      "terminal.integrated.fontFamily" = "MesloLGL Nerd Font";
+    };
   };
 }
