8c86ac847c06c4bf31db7a96b2aee40377622e0d
Alex Colby <alexanderdcolby@gmail.com>
2025-02-20 18:09:10 +0000

    tweak(desktop): removes lilypad extension
    This extension isn't as useful now that:
    1. I'm pretty sure I'll only be using 2 top bar extensions
    2. `hide-top-bar` keeps these extensions out the ways anyways

--- modules/desktop/extensions.nix
+++ modules/desktop/extensions.nix
@@ -2,7 +2,6 @@
 {
   home.packages = with pkgs.gnomeExtensions; [
     todotxt
-    lilypad
     clipboard-indicator
     hide-top-bar
   ];
@@ -12,23 +11,12 @@
     disable-user-extensions = false;
     enabled-extensions = with pkgs.gnomeExtensions; [
       todotxt.extensionUuid
-      lilypad.extensionUuid
       clipboard-indicator.extensionUuid
       hide-top-bar.extensionUuid
     ];
   };
 
   dconf.settings = {
-    "org/gnome/shell/extensions/lilypad" = {
-      show-icons = true;
-      reorder = true;
-      ignored-order = "@as []";
-      lilypad-order = [
-        "todoTxt"
-        "clipboardIndicator"
-      ];
-      rightbox-order = [ "lilypad" ];
-    };
     "org/gnome/shell/extensions/hidetopbar" = {
       mouse-sensitive = true;
     };
