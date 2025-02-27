7ecb708b2b5732f28e61ec2a3ac88432d83df753
Alex Colby <alexanderdcolby@gmail.com>
2025-02-27 21:25:59 +0000

    tweak(vscode): removes mentions of vscode chat

--- modules/editors/vscode/visuals.nix
+++ modules/editors/vscode/visuals.nix
@@ -2,5 +2,6 @@
   programs.vscode.mutUserSettings = {
     "terminal.integrated.fontFamily" = "MesloLGL Nerd Font";
     "workbench.colorTheme" = "Atom One Dark";
+    "chat.commandCenter.enabled" = false;
   };
 }
