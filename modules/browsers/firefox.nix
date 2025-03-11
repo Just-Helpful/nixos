dbb674771af572d6a3dc58e27c6cedb87a0abdac
Alex Colby <alexanderdcolby@gmail.com>
2025-03-11 14:19:06 +0000

    tweak(browsers): reverts to ESR firefox
    There were some (hopefully temporary) issues with getting
    nightly firefox to respect `policies.json`

--- modules/browsers/firefox.nix
+++ modules/browsers/firefox.nix
@@ -1,12 +1,10 @@
 {
   pkgs,
-  inputs,
   ...
 }:
 {
   programs.firefox = {
     enable = true;
-    package = inputs.firefox.packages.${pkgs.system}.firefox-nightly-bin;
 
     policies = {
       DisablePocket = true;
