82edf1a503c548d836ae8ef46b223a9359e940fa
Alex Colby <alexanderdcolby@gmail.com>
2025-03-15 21:13:19 +0000

    feat(tools): adds service to mount `~/Backups` dir

--- modules/tools/rclone.nix
+++ modules/tools/rclone.nix
@@ -1,4 +1,15 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ rclone ];
+
+  # Set up a mounted directory for backups
+  # You'll need to set up an rclone remote called `Backups` for this.
+  systemd.user.services.rclone-mount-backups = {
+    Unit.Description = "Mount ~/Backups directory on startup";
+    Install.WantedBy = [ "default.target" ];
+    Service.ExecStart = "${pkgs.writeShellScript "rclone-mount-backups" ''
+      #!/run/current-system/sw/bin/bash
+      ${pkgs.rclone}/bin/rclone mount --vfs-cache-mode writes Backups:Backups /home/alexc/Backups
+    ''}";
+  };
 }
