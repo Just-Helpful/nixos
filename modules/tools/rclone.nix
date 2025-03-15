1b5a58c704d9438ea8df9540c7054041d084dea3
Alex Colby <alexanderdcolby@gmail.com>
2025-03-15 21:58:34 +0000

    tweak(tools): temporarily disables `rclone`
    The sync solution I was planning on using was extremely
    temperamental and I'll just be using a physical backup
    for the time being, despite the inconvenience

--- modules/tools/rclone.nix
+++ modules/tools/rclone.nix
@@ -1,15 +1,40 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [ rclone ];
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.programs.rclone;
+in
+{
+  options = {
+    programs.rclone = {
+      enable = lib.mkEnableOption "rclone";
 
-  # Set up a mounted directory for backups
-  # You'll need to set up an rclone remote called `Backups` for this.
-  systemd.user.services.rclone-mount-backups = {
-    Unit.Description = "Mount ~/Backups directory on startup";
-    Install.WantedBy = [ "default.target" ];
-    Service.ExecStart = "${pkgs.writeShellScript "rclone-mount-backups" ''
-      #!/run/current-system/sw/bin/bash
-      ${pkgs.rclone}/bin/rclone mount --allow-non-empty --vfs-cache-mode writes Backups:Backups /home/alexc/Backups
-    ''}";
+      backup-service = {
+        enable = lib.mkEnableOption "mounting a backup directory via rclone";
+        mount_path = lib.mkOption {
+          type = lib.types.str;
+          default = "/home/alexc/Backups";
+          description = "Path at which to mount the backups directory";
+        };
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [ rclone ];
+
+    # Set up a mounted directory for backups
+    # You'll need to set up an rclone remote called `Backups` for this.
+    systemd.user.services.rclone-mount-backups = lib.mkIf cfg.backup-service.enable {
+      Unit.Description = "Mount ~/Backups directory on startup";
+      Install.WantedBy = [ "default.target" ];
+      Service.ExecStart = "${pkgs.writeShellScript "rclone-mount-backups" ''
+        #!/run/current-system/sw/bin/bash
+        ${pkgs.rclone}/bin/rclone mount --allow-non-empty --vfs-cache-mode writes Backups:Backups ${cfg.backup-service.mount_path}
+      ''}";
+    };
   };
 }
