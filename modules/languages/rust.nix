adbeaee65b7aa565a77ffa24b85bdeb7d5db1ae6
Alex Colby <alexanderdcolby@gmail.com>
2025-03-16 23:24:13 +0000

    fix(langs): removes customisation of `RUSTUP_HOME`
    This was causing the tests the `rustup` runs on installation
    to fail, due to some daft attempts to write to the same folder
    from within tests (which it should **not**, tests shouldn't be stateful)

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs; [
     gcc
     (rustup.overrideAttrs {
-      RUSTUP_HOME = "/home/alexc/.languages/rust/rustup";
+      # RUSTUP_HOME = "/home/alexc/.languages/rust/rustup";
       CARGO_HOME = "/home/alexc/.languages/rust/cargo";
     })
   ];
