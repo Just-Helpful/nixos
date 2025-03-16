fed53587f8d907ea5df47b30e494afe350b2e095
Alex Colby <alexanderdcolby@gmail.com>
2025-03-16 23:39:10 +0000

    fix(cli): excludes hardware scan files from checks
    As these are generated files, I don't want to check their formatting
    as I have absolutely no plans to edit them whatsoever.

--- .github/config/treefmt.nix
+++ .github/config/treefmt.nix
@@ -1,6 +1,9 @@
 {
   projectRootFile = "flake.nix";
-  settings.global.excludes = [ "README.md" ];
+  settings.global.excludes = [
+    "README.md"
+    "**/hardware.nix"
+  ];
   settings.global.on-unmatched = "info";
 
   programs.nixfmt.enable = true;
