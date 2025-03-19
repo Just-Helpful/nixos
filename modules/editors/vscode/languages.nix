2628825751461230f9acbe9dc53f4c4c05b74c06
Alex Colby <alexanderdcolby@gmail.com>
2025-03-19 15:38:42 +0000

    tweak(editors): disables inlay type hints in rust

--- modules/editors/vscode/languages.nix
+++ modules/editors/vscode/languages.nix
@@ -6,5 +6,7 @@
     "[json]" = {
       "editor.defaultFormatter" = "vscode.json-language-features";
     };
+    "rust-analyzer.inlayHints.typeHints.enable" = false;
+    "rust-analyzer.inlayHints.parameterHints.enable" = false;
   };
 }
