033929a92554acd585763ebc5a5b1c98c22240bb
Alex Colby <alexanderdcolby@gmail.com>
2025-04-06 22:10:10 +0100

    fix(nixos): adds networking rules

--- hosts/nixos/default/network.nix
+++ hosts/nixos/default/network.nix
@@ -9,6 +9,11 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  networking.networkmanager.unmanaged = [
+    "*"
+    "except:type:wwan"
+    "except:type:gsm"
+  ];
 
   # Enable IOS interfacing
   services.usbmuxd.enable = true;
