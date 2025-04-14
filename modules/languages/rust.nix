efd3cffde3cb797a932c439a8ae1ffd9afb3f9aa
Alex Colby <alexanderdcolby@gmail.com>
2025-04-14 17:19:13 +0100

    fix(langs): properly sets environment variables
    Previously these were erroneously being set with the `rustup` install
    rather than in the terminal in general, where they were being readily
    ignored.
    
    This also had the rather fun side effect of breaking the cache for
    `rustup` installs, requiring it to be manually compiled.

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -1,13 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   imports = [
     ./c.nix
   ];
 
+  home.sessionVariables = {
+    RUSTUP_HOME = "${config.home.homeDirectory}/.languages/rust/rustup/";
+    CARGO_HOME = "${config.home.homeDirectory}/.languages/rust/cargo/";
+  };
+
   home.packages = with pkgs; [
-    (rustup.overrideAttrs {
-      # RUSTUP_HOME = "/home/alexc/.languages/rust/rustup";
-      CARGO_HOME = "/home/alexc/.languages/rust/cargo";
-    })
+    rustup
   ];
 }
