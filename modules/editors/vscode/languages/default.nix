6fa0906315003543977b74fd48160f22297cde6f
Alex Colby <alexanderdcolby@gmail.com>
2025-04-30 20:16:05 +0100

    feat(vscode/langs): adds typescript formatting

--- modules/editors/vscode/languages/default.nix
+++ modules/editors/vscode/languages/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./json.nix
     ./rust.nix
+    ./typescript.nix
   ];
 }
---
+++ modules/editors/vscode/languages/typescript.nix
@@ -0,0 +1,7 @@
+{
+  programs.vscode.mutUserSettings = {
+    "[typescript]" = {
+      "editor.defaultFormatter" = "esbenp.prettier-vscode";
+    };
+  };
+}
