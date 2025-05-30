4a4aaf06f52feb7a46178d05005ef7a853d16319
Alex Colby <alexanderdcolby@gmail.com>
2025-05-30 22:13:33 +0100

    fix(hosts): refactor to avoid deprecation

--- hosts/nixos/default/services.nix
+++ hosts/nixos/default/services.nix
@@ -1,7 +1,5 @@
 {
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
+  services = {
     # Enable the GNOME Desktop Environment.
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
