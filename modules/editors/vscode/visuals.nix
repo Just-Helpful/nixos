ff7380c3498c986d49101375adfc33c90d2f6b58
Alex Colby <alexdcolby@pm.me>
2025-07-28 20:29:35 +0100

    tweak(vscode): disables AI sidebar by default

--- modules/editors/vscode/visuals.nix
+++ modules/editors/vscode/visuals.nix
@@ -3,5 +3,6 @@
     "terminal.integrated.fontFamily" = "MesloLGL Nerd Font";
     "workbench.colorTheme" = "Atom One Dark";
     "chat.commandCenter.enabled" = false;
+    "workbench.secondarySideBar.defaultVisibility" = false;
   };
 }
