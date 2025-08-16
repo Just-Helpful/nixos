6a93e9ecd16f9f942236d7acdf2557e6ea1e0898
Alex Colby <alexdcolby@pm.me>
2025-08-16 22:05:06 +0100

    tweak(desk): updates keybinds for window menu

--- modules/desktop/keybinds.nix
+++ modules/desktop/keybinds.nix
@@ -4,6 +4,10 @@
     "org/gnome/desktop/wm/keybindings" = {
       # Close windows easier
       close = [ "<Super>q" ];
+      # Use "<Super>" for window actions, by default
+      activate-window-menu = "<Super>space";
+      switch-input-source = "";
+      switch-input-source-backward = "";
       # Disable "move-monitor" keybinds
       move-to-monitor-up = [ ];
       move-to-monitor-down = [ ];
