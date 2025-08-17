a0ce69caba225a7b2d6825b3e900a6f78ec76eb2
Alex Colby <alexdcolby@pm.me>
2025-08-17 22:28:16 +0100

    feat(desk): uses open bar instead of hiding it
    I've found if I make the bar consistent enough with the rest of theming
    I'm actually okay with it being visible at all times

--- modules/desktop/extensions/top-bar.nix
+++ modules/desktop/extensions/top-bar.nix
@@ -1,14 +1,52 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs.gnomeExtensions; [
-    hide-top-bar
+    open-bar
   ];
 
   dconf.enable = true;
-  dconf.settings."org/gnome/shell" = {
-    disable-user-extensions = false;
-    enabled-extensions = with pkgs.gnomeExtensions; [
-      hide-top-bar.extensionUuid
-    ];
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = with pkgs.gnomeExtensions; [
+        open-bar.extensionUuid
+      ];
+    };
+
+    "org/gnome/shell/extensions/openbar" = {
+      bartype = "TriLands";
+      margin = 0;
+
+      # Transparent background, like nvim
+      bgalpha = 0.7;
+      isalpha = 0.7;
+
+      # Match borders on apps
+      bwidth = 1;
+      bradius = 12;
+      bcolor = [
+        "0.239"
+        "0.220"
+        "0.275"
+      ];
+      iscolor = [
+        "0.239"
+        "0.220"
+        "0.275"
+      ];
+      neon = false;
+
+      # Match highlights around apps
+      autohg-bar = false;
+      dark-hcolor = [
+        "0.208"
+        "0.518"
+        "0.894"
+      ];
+      heffect = true;
+
+      # Don't let open bar fiddle with menus
+      menustyle = false;
+    };
   };
 }
