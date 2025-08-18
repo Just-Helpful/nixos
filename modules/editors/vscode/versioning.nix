38a29a3875824464168102dbb7eb541f2eb6bf7b
Alex Colby <alexdcolby@pm.me>
2025-08-18 19:09:42 +0100

    feat(vscode): adds task for git graph
    This relies on the git graph extension

--- modules/editors/vscode/versioning.nix
+++ modules/editors/vscode/versioning.nix
@@ -10,4 +10,26 @@
     "git.enableCommitSigning" = true;
     "git.useCommitInputAsStashMessage" = false;
   };
+
+  programs.vscode.mutUserTasks = [
+    {
+      label = "Open Git Graph";
+      command = "\${command:git-graph.view}";
+    }
+    {
+      label = "Pin Tab";
+      command = "\${command:workbench.action.pinEditor}";
+    }
+    {
+      label = "Pin Git Graph";
+      dependsOrder = "sequence";
+      dependsOn = [
+        "Open Git Graph"
+        "Pin Tab"
+      ];
+      runOptions = {
+        runOn = "folderOpen";
+      };
+    }
+  ];
 }
