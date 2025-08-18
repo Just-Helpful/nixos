274b37739434b11362b0d7770caa446af99f9c8d
Alex Colby <alexdcolby@pm.me>
2025-08-18 18:57:08 +0100

    feat(vscode): adds mutable user task support

--- modules/editors/vscode/default.nix
+++ modules/editors/vscode/default.nix
@@ -19,19 +19,39 @@ in
     ./versioning.nix
   ];
 
-  options.programs.vscode.mutUserSettings = lib.options.mkOption {
-    inherit (jsonFormat) type;
-    default = { };
-    example = lib.options.literalExpression ''
-      {
-        "files.autoSave" = "off";
-        "[nix]"."editor.tabSize" = 2;
-      }
-    '';
-    description = ''
-      Configuration written to Visual Studio Code's
-      {file}`settings.json`.
-    '';
+  options.programs.vscode = {
+    mutUserSettings = lib.options.mkOption {
+      inherit (jsonFormat) type;
+      default = { };
+      example = lib.options.literalExpression ''
+        {
+          "files.autoSave" = "off";
+          "[nix]"."editor.tabSize" = 2;
+        }
+      '';
+      description = ''
+        Configuration written to Visual Studio Code's
+        {file}`settings.json`.
+      '';
+    };
+
+    mutUserTasks = lib.options.mkOption {
+      inherit (jsonFormat) type;
+      default = { };
+      example = lib.options.literalExpression ''
+        [
+          {
+            type = "shell";
+            label = "Hello task";
+            command = "hello";
+          }
+        ];
+      '';
+      description = ''
+        Configuration written to Visual Studio Code's
+        {file}`tasks.json`.
+      '';
+    };
   };
 
   config = {
@@ -42,6 +62,13 @@ in
     home.mutFile = {
       "${config.xdg.configHome}/Code/User/settings.json".source =
         jsonFormat.generate "vscode-mut-user-settings" cfg.mutUserSettings;
+
+      "${config.xdg.configHome}/Code/User/tasks.json".source =
+        jsonFormat.generate "vscode-mut-user-tasks"
+          {
+            version = "2.0.0";
+            tasks = cfg.mutUserTasks;
+          };
     };
   };
 }
