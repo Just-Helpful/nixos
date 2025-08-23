da964140747c0466ec772a8ce7039f98a75ece9d
Alex Colby <alexdcolby@pm.me>
2025-08-23 14:42:36 +0100

    tweak(desk): prefers fully transparent background

--- modules/desktop/extensions/top-bar.nix
+++ modules/desktop/extensions/top-bar.nix
@@ -18,18 +18,24 @@
       margin = 0.0;
 
       # Transparent background, like nvim
-      bgcolor = [
-        "0.239"
-        "0.220"
-        "0.275"
-      ];
-      bgalpha = 0.7;
+      bgalpha = 0.0;
       iscolor = [
         "0.141"
         "0.122"
         "0.192"
       ];
       isalpha = 0.7;
+
+      # In maximum mode, we make the background translucent
+      wmaxbar = true;
+      buttonbg-wmax = true;
+      border-wmax = true;
+      bgcolor-wmax = [
+        "0.239"
+        "0.220"
+        "0.275"
+      ];
+      bgalpha-wmax = 0.7;
 
       # Match borders on apps
       bwidth = 1.0;
