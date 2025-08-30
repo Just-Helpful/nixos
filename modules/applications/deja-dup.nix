4b89b7a11d14cf6b58a5cb2bd6badc0ce9c93c9f
Alex Colby <alexdcolby@pm.me>
2025-08-30 21:13:29 +0100

    feat(backups): configures default folders

--- modules/applications/deja-dup.nix
+++ modules/applications/deja-dup.nix
@@ -2,10 +2,22 @@
   dconf.enable = true;
   dconf.settings = {
     "org/gnome/deja-dup" = {
-      "periodic" = true;
-      "periodic-period" = 1;
-      "delete-after" = 182;
-      "tool-when-new" = "restic";
+      periodic = true;
+      periodic-period = 1;
+      delete-after = 182;
+      tool-when-new = "restic";
+
+      include-list = [
+        "/home/default/.local/share/keyrings"
+        "/home/default/.local/share/PrismLauncher"
+        "/home/default/.local/share/todo.txt"
+        "/home/default/.mozilla/firefox/alexc/bookmarkbackups"
+        "/home/default/Documents"
+        "/home/default/Music"
+        "/home/default/Pictures"
+      ];
+
+      exclude-list = [ ];
     };
   };
 }
