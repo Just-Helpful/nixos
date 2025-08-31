8a5a6f95bc8da0116ec2f7d342fbf18d4b52f213
Alex Colby <alexdcolby@pm.me>
2025-08-31 18:22:43 +0100

    Revert "Merge branch 'main' of ssh://github.com/Just-Helpful/nixos"
    This reverts commit da6f4e0ed5d46a35798be711acae2af97ac18ce2, reversing
    changes made to c0833e71d879f6fe52704f0bb3a50c861b3798bd.

--- hosts/nixos/default/localisation.nix
+++ hosts/nixos/default/localisation.nix
@@ -16,6 +16,12 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
   # Configure console keymap
   console.keyMap = "uk";
 }
