689bc02f0dde1516bad2706e2978015c89378bac
Alex Colby <alexdcolby@pm.me>
2025-09-02 22:11:30 +0100

    tweak(tools): provides default ssh config
    This resolves deprecated default warnings

--- modules/terminal/tools/ssh.nix
+++ modules/terminal/tools/ssh.nix
@@ -1,4 +1,18 @@
 {
   programs.ssh.enable = true;
+  programs.ssh.enableDefaultConfig = false;
+  programs.ssh.matchBlocks."*" = {
+    forwardAgent = false;
+    addKeysToAgent = "no";
+    compression = false;
+    serverAliveInterval = 0;
+    serverAliveCountMax = 3;
+    hashKnownHosts = false;
+    userKnownHostsFile = "~/.ssh/known_hosts";
+    controlMaster = "no";
+    controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "no";
+  };
+
   services.ssh-agent.enable = true;
 }
