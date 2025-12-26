3f2da246d58c68ab19518fbf9e68fb032ec2e645
Alex Colby <alexdcolby@pm.me>
2025-12-26 16:18:08 +0000

    feat(desk): adds default config for `cronomix`

--- modules/desktop/extensions/cronomix.nix
+++ modules/desktop/extensions/cronomix.nix
@@ -1,6 +1,11 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  jsonFormat = pkgs.formats.json { };
+in
 {
-  home.packages = with pkgs.gnomeExtensions; [ cronomix ];
+  home.packages = with pkgs.gnomeExtensions; [
+    cronomix
+  ];
 
   dconf.enable = true;
   dconf.settings."org/gnome/shell" = {
@@ -9,4 +14,49 @@
       cronomix.extensionUuid
     ];
   };
+
+  # cronomix config is done via `*.json` files in `~/.config`
+  home.mutFile."${config.xdg.configHome}/cronomix/global.json".source =
+    jsonFormat.generate "cronomix-global"
+      {
+        values = {
+          todo = {
+            tag = "boolean";
+            value = true;
+          };
+          alarm = {
+            tag = "boolean";
+            value = false;
+          };
+          timer = {
+            tag = "boolean";
+            value = true;
+          };
+          pomodoro = {
+            tag = "boolean";
+            value = false;
+          };
+          stopwatch = {
+            tag = "boolean";
+            value = false;
+          };
+          flashcards = {
+            tag = "boolean";
+            value = false;
+          };
+          theme_file = {
+            tag = "file";
+            value = "";
+            start = "${config.xdg.dataHome}/gnome-shell/extensions/${pkgs.gnomeExtensions.cronomix.extensionUuid}/data/themes/";
+          };
+          lazy_list_page_size = {
+            tag = "number";
+            value = 20;
+            range = [
+              1
+              100000
+            ];
+          };
+        };
+      };
 }
