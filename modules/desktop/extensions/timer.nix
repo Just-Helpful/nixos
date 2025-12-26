3ff55cd679cae166c32b53348351cbdec074bee6
Alex Colby <alexdcolby@pm.me>
2025-12-26 12:53:39 +0000

    feat(desk): enables timer in top bar

--- modules/desktop/extensions/timer.nix
+++ modules/desktop/extensions/timer.nix
@@ -3,4 +3,12 @@
   home.packages = with pkgs.gnomeExtensions; [
     timer
   ];
+
+  dconf.enable = true;
+  dconf.settings."org/gnome/shell" = {
+    disable-user-extensions = false;
+    enabled-extensions = with pkgs.gnomeExtensions; [
+      timer.extensionUuid
+    ];
+  };
 }
