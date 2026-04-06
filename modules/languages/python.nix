44e93046a9212b65dcfeb32dc733576929bc4a44
Alex Colby <alexdcolby@pm.me>
2026-04-06 21:41:28 +0100

    fix(shell): disables double virtual environment prompt

--- modules/languages/python.nix
+++ modules/languages/python.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ uv ];
+
+  # disable double prompt for venvs
+  programs.nushell.extraConfig = ''
+    $env.VIRTUAL_ENV_DISABLE_PROMPT = true;
+  '';
 }
