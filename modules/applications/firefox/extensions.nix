ac9c96c4b273024f53f854838c49c60e00ba6839
Alex Colby <alexdcolby@pm.me>
2026-05-25 10:58:00 +0100

    tweak: removes fox tab

--- modules/applications/firefox/extensions.nix
+++ modules/applications/firefox/extensions.nix
@@ -1,10 +1,5 @@
 {
   programs.firefox.policies.ExtensionSettings = {
-    # FoxyTab
-    "foxytab@eros.man" = {
-      installation_mode = "normal_installed";
-      install_url = "https://addons.mozilla.org/firefox/downloads/latest/foxytab/latest.xpi";
-    };
     # Proton Pass
     "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
       installation_mode = "normal_installed";
