f05993e2b0252a86506604b64037d2dce3d0405e
Alex Colby <alexdcolby@pm.me>
2026-06-30 23:16:20 +0100

    fix: lowers file descriptor usage for vscode

--- modules/editors/vscode/files.nix
+++ modules/editors/vscode/files.nix
@@ -11,5 +11,13 @@
       "**/.svelte-kit" = true;
     };
     "explorer.confirmDelete" = false;
+
+    # stop vscode watching *so many* files
+    "files.watcherExclude" = {
+      "**/.git/objects/**" = true;
+      "**/.git/subtree-cache/**" = true;
+      "**/node_modules/*/**" = true;
+      "**/target/**" = true;
+    };
   };
 }
