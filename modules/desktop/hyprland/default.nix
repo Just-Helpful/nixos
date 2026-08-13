97f263b8cb52fe4a1ac8d59d0d6d838854b2310a
Alex Colby <alexdcolby@pm.me>
2026-08-13 21:54:18 +0100

    feat: converts to a home manager install

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./dunst.nix
     ./flameshot.nix
@@ -12,4 +12,17 @@
     ./starship.nix
     ./waybar/waybar.nix
   ];
+
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
+    enable = true;
+    # The hyprland package to use
+    package = pkgs.hyprland;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+
+    # Optional
+    # Whether to enable hyprland-session.target on hyprland startup
+    systemd.enable = true;
+  };
 }
